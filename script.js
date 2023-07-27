e080cbccda310281a30726113b19a4f92ae95377
fitzTurtle <137014500+FitzTurtle@users.noreply.github.com>
2023-07-27 15:39:20 -0400

    created playRound function

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const choices = ["Rock","Paper","Scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)];
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    let lowerPlayer = playerSelection.toLowerCase();
+    let lowerComp = computerSelection.toLowerCase();
+    let playerVictory = "You won! " playerSelection + " beats " + computerSelection;
+    let compVictory =  "You lost! " computerSelection + " beats " + playerSelection;
+
+    if(lowerPlayer == lowerComp) {
+        return "You tied! "+ playerSelection + " ties with " + computerSelection; 
+    }
+    else if(lowerPlayer == "rock" && lowerComp == "paper") {
+        return compVictory;
+    } else if(lowerPlayer == "paper" && lowerComp == "scissors"){
+        return compVictory;
+    }
+    else if(lowerPlayer == "scissors" && lowerComp == "rock") {
+        return compVictory;
+    } else {
+        return playerVictory;
+    }
+
+
+}
