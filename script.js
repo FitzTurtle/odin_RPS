9dba6d15af71ae62a17bbe90be2a9398cbbb4213
fitzTurtle <137014500+FitzTurtle@users.noreply.github.com>
2023-07-27 16:10:12 -0400

    Added select valid function to check for player typos in choices

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["Rock","Paper","Scissors"];
+let playerPoints;
+let compPoints;
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*3)];
@@ -15,15 +17,52 @@ function playRound(playerSelection, computerSelection) {
         return "You tied! "+ playerSelection + " ties with " + computerSelection; 
     }
     else if(lowerPlayer == "rock" && lowerComp == "paper") {
+        compPoints++;
         return compVictory;
     } else if(lowerPlayer == "paper" && lowerComp == "scissors"){
+        compPoints++;
         return compVictory;
     }
     else if(lowerPlayer == "scissors" && lowerComp == "rock") {
+        compPoints++;
         return compVictory;
     } else {
+        playerPoints++;
         return playerVictory;
     }
+}
 
+function game() {
+    playerPoints = 0;
+    compPoints = 0;
+
+    while(playerPoints + compPoints < 5){
+        console.log(playRound(selectValid(),getComputerChoice()));
+    }
+
+    if(playerPoints > compPoints){
+        console.log("Congrats! you won with "+playerPoints+" total games won out of 5");
+    } else {
+        console.log("You lose! The computer won " + compPoints + " out of 5 games");
+    }
+}
+
+function selectValid() {
+    valid = false;
+    let playerSelection;
+    while (!valid) {
+        playerSelection = prompt().toLowerCase();
+
+        switch(playerSelection){
+            case "rock":
+            case "paper":
+            case "scissors":
+                valid = true;
+                break;
+            default:
+                console.log("please type rock paper or scissors");
+        }
+    }
+    return playerSelection;
 
 }
